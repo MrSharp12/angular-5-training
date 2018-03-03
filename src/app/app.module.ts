1777d17b7e564132591a7a26b39d328136218936
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-03 15:24:39 -0500

    created servers, passed app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';//this is how angular knows we created a new component
 
 
-@NgModule({
+@NgModule({//angular decorator
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,//let's angular know we created a new element
+    ServersComponent//added automatically by the cli
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';//import component, since TS doesn't know it from the start
+
+@Component({//passing a JS object to config this, stored as metadata
+    selector:'app-server',//as a selector, can be used in other component's html files
+    templateUrl: './server.component.html'
+})//component decorator, let's angular know that this is a component
+export class ServerComponent { //exporting this component, so it can be used elsewhere
+
+}
