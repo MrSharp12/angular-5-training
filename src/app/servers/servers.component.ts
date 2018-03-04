135625374f67db6c16d3e4f18b014dc8ed3162b7
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-03 19:31:15 -0500

    set up template in server

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: '<app-server></app-server><app-server></app-server>',
+  //component must have either template or templateUrl, must have one at all times
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
