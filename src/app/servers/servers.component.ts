6acc39dc3beef500597e78aaff64e4ea15385b77
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-03 23:40:00 -0500

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,13 +5,19 @@ import { Component, OnInit } from '@angular/core';
   //it now has to be selected as an attribute
   // selector: '.app-servers',now it is selected by class
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
   //component must have either template or templateUrl, must have one at all times
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
