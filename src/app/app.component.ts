5bcc66ba8885c6cf27f3116c9996afaa762ec384
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-03 21:52:29 -0500

    understanding component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,8 @@ import { Component } from '@angular/core';
   //styleUrls: ['./app.component.css'], this is an array because we can point to multiple stylesheets
   styles: [`
     h3 { color: pink }
-  `]//another way to style, also an array but of strings, where you can define the style
-  //also, backticks used to create a multi-line statment
+  `]
 })
 export class AppComponent {
   
 }
-//when to use file css vs inline?
-//if we have lots of style code, go for file
-//if just a line or two, go inline ts file
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',by placing the component selector in square brackest
+  //it now has to be selected as an attribute
+  // selector: '.app-servers',now it is selected by class
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   //component must have either template or templateUrl, must have one at all times
