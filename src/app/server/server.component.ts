2e2e1b129ec5bf6beed47f7be4edde5ee310e76a
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-04 22:25:30 -0500

    ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';//import component, since TS doesn't know it from the start
+import { Component } from '@angular/core';
 
-@Component({//passing a JS object to config this, stored as metadata
-    selector:'app-server',//as a selector, can be used in other component's html files
-    templateUrl: './server.component.html'
-})//component decorator, let's angular know that this is a component
-export class ServerComponent { //exporting this component, so it can be used elsewhere
+@Component({
+    selector:'app-server',
+    templateUrl: './server.component.html',
+    styles: [`
+        .online{
+            color: white;
+        }
+    `]
+})
+export class ServerComponent { 
     serverId = 10;
     serverStatus = 'offline';
-    //these properties will be output to the template
+    
 
-    constructor() {//constructor is a method each ts has
+    constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
 
